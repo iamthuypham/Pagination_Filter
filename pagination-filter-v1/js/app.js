76303b238f25c0e7e04a88cc3e5ebe528beceda2
Thuy Pham <tpham045@hotmail.com>
2016-05-04 22:45:30 -0700

    Update the latest version from local
    1. Apply contains()
    2. Reorganie Searching Flow and Add comments

--- pagination-filter-v1/js/app.js
+++ pagination-filter-v1/js/app.js
@@ -1,78 +1,65 @@
-//Create the Search field and Search button
+//Create the Search field
 $(".page-header").append("<div class=\"student-search\">  <input placeholder=\"Search for students...\">  <button>Search</button></div>");
-//Create array of objects which includes firstName, lastName, and email. This array is used for searching
-var array=[];
-$(".student-details").each(function(){
-  array.push({
-    firstName:$(this).children("h3").text().split(" ")[0], //detect the firstName based on h3 tag, split to an array, and get position 0
-    lastName:$(this).children("h3").text().split(" ")[1],//detect the firstName based on h3 tag, split to an array, and get position 1
-    email:$(this).children("span").text()
-  })
-})
-//Function1: create page links based on currentList. 
-//Variable currentList could be full list of student $(".student-item") or search result list $(".found") (please read Function 4)
+
+//Function1: create page links based on number of students
 function createPageLink(currentList){
-    maxPage = Math.ceil($(currentList).size()/10); //calculate number of pages = (number of students in current list / number of student per page)
-    $(".pagination").remove();//remove any existed page link so that able to apply a new set
+    totalPage = Math.ceil($(currentList).size()/10);
+    $(".pagination").remove();
     var pageLink = "<div class=\"pagination\"><ul>"
-            + "<li><a class=\"active\" href=\"#\">1</a></li>"; //setup page link '1' as active by default
-    for (i=2; i<=maxPage; i++) {
-      pageLink += "<li><a href=\"#\">"+ i +"</a></li>"; }; //loop thru next page links 2,3,4,... until meet maximum pages
+            + "<li><a class=\"active\" href=\"#\">1</a></li>";
+    for (i=2; i<=totalPage; i++) {
+      pageLink += "<li><a href=\"#\">"+ i +"</a></li>"; };
     pageLink += "</ul></div>";
     $(".page").append(pageLink);//Add page links to index.html
 };
-//Function2: loading student list based on active page, currentList, numberPerPage
+//Function2: loading based on active page, currentList, numberPerPage
 function loadItem(currentList){
-  var x = $(".active").text(); //if you are in page 2 (which is 'active' page, get the number 2
-  var start = x * 10 - 10; //detect where to start in the list, start = 2 * 10 - 10 = 10
-  var end = x * 10;// detect where to end in the list, end = 2 * 10 = 20
-  $(".student-item").hide(); //hide all current existed list
-  $(currentList).slice(start,end).show();//display the current list from position 10th to position 20th
+  var x = $(".active").text();
+  var start = x * 10 - 10; //where detected currentList starts
+  var end = x * 10;// where detected currentList ends
+  $(".student-item").hide();
+  $(currentList).slice(start,end).show();//display 10
 };
-//Function3: Update CSS when click on certain page link. Example: You are on page 2 and click on page 3.
+//Function3: Update CSS when click on certain page link
 function pageClick(currentList){
-  $( "a" ).click(function(){
-    $( "a" ).removeClass("active");//remove the 'active' class from page 2
-    $( this ).addClass("active");//add the 'active' class to page 3
-    loadItem(currentList); //reload the current list whenever click on new page
+  $( ".pagination a" ).click(function(){
+    $( ".active" ).removeClass("active");//switch active class for CSS update
+    $( this ).addClass("active");
+    loadItem(currentList); //reload item after every click
   });
 }
 //Function4: Search item and add class "found"
-function searchItem(){
-  keyWord = $("input").val() //detect text that users input
-  var result = 0;
-  $(".found").toggleClass("found", false);//remove any existed 'found' class so that every keyup function will not load the previous results
-  for(var i=0; i<array.length; i++) {//loop thru each object of the array
-    for(var key in array[i]) {//loop thru each key of an object
-      if(array[i][key].indexOf(keyWord)!=-1) {//if there is a match
-        $(".student-item").eq(i).addClass("found");//then add that student item a class 'found'
-        result ++;
-      }
-    }
-  }
-  return result; //Variable result will be used for no matches scenarios
+function searchItem(keyWord){
+  $(".found").toggleClass("found", false);
+  return   $(".student-details:contains("+keyWord+")").parent().addClass("found");
 };
-/******LOAD DEFAULT PAGE**********/
-createPageLink(".student-item");//create page links for full list of student
-loadItem(".student-item");//load full list of student based on page number
-pageClick(".student-item"); 
+//LOAD DEFAULT PAGE // when load page, create page link and load all students
+createPageLink(".student-item" );
+loadItem(".student-item" );
+pageClick(".student-item" );
 
-/*******LOAD SEARCHING PAGE********/
-$("input").keyup(function(){ //Every key up, program search and find the match
-  searchItem();
-  var match = searchItem();
-  if (match != 0){ //If there is one or more matches
-    $(".unmatchMessage").hide(); //hide any existed unmatch message so that program won't load the message every single keyup
-    $(".student-list").show();//show whole student list first 
-    createPageLink(".found");//and then create page link for the search result list 
-    loadItem(".found");//reload all items in the search result list
-    pageClick(".found");
-  } else { //If there is no match
-    $(".student-list").hide();//hide all student list
-    $(".pagination").hide();//hide any page link
-    var unMatch = "<div class = \"unmatchMessage\">There is no match</div>"; //prepare unmatch message
-    $(".unmatchMessage").empty() //Empty the message every keyup first
-    $(".page").append(unMatch);// then add the message
+//LOAD SEARCHING PAGE
+$("input").keyup(function(){
+  keyWord = $("input").val(); //detect input keyword
+  if (keyWord != ""){ //if keyword is not blank
+    var match = searchItem(keyWord); //start searching
+    if (match.size() > 0) { //if found one or more matches
+      $(".unmatchMessage").remove();//remove any existed message
+      createPageLink(".found"); //create page link and load only students that found from searching
+      loadItem(".found");
+      pageClick(".found");
+    } else {//If found no match
+      $(".student-item").hide(); //Hide all students
+      $(".pagination").remove();//Remove the pagination
+      $(".unmatchMessage").remove() //Remove any existed message whenever keyup. This will avoid create multiple <div> for unmatch message if users type several unmatch keyword such as "1111111111"
+      var noMatch = "<div class=\"unmatchMessage\">There is no match</div>";
+      $(".page").append(noMatch);
+    }
+  } else {//If the keyword is blank when key up (this usually happens when users type some keyword and backspace the whole keyword)
+    $(".unmatchMessage").remove(); //remove any existed message
+    createPageLink(".student-item");//create page link and load all students
+    loadItem(".student-item");
+    pageClick(".student-item");
   }
 })
 
